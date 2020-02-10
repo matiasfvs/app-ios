e7e7ceca68c792617ec29b929c19bec255b7e867
Roberto Osorio <robertoosorio.s@gmail.com>
2020-02-10 19:49:19 -0300

    Initial Commit

---
+++ MatuSampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MatuSampleApp
+//
+//  Created by Roberto Osorio Sanhueza on 2/10/20.
+//  Copyright © 2020 Roberto Osorio Sanhueza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MatuSampleAppTests/MatuSampleAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  MatuSampleAppTests.swift
+//  MatuSampleAppTests
+//
+//  Created by Roberto Osorio Sanhueza on 2/10/20.
+//  Copyright © 2020 Roberto Osorio Sanhueza. All rights reserved.
+//
+
+import XCTest
+@testable import MatuSampleApp
+
+class MatuSampleAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
